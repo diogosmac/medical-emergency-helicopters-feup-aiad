0242c7f99f34eb5517f63c29825508da0331e9c9
Diogo Machado <diogo.smac@gmail.com>
2020-11-11 18:03:13 +0000

    Added Logger class, and changed initialization of Jade class to receive arguments from the command line

--- src/Jade.java
+++ src/Jade.java
@@ -3,11 +3,29 @@ import jade.core.ProfileImpl;
 import jade.core.Profile;
 import jade.wrapper.AgentContainer;
 import jade.wrapper.StaleProxyException;
+import utils.Logger;
 import utils.ScenarioReader;
 
 public class Jade {
 
     public static void main(String[] args) {
+
+        boolean testOnly = false;
+
+        if (args.length < 1 || args.length > 2) {
+            System.out.println(
+                    "Usage: java MedicalEmergencyHelicopters <json-file> [ <test> ]\n" +
+                    "       json-file:  path to file containing helicopters, hospitals and patients\n" +
+                    "       test:       FALSE (default) if logger should document execution, TRUE if testing only"
+            );
+            System.exit(1);
+        }
+
+        if (args.length == 2) {
+            testOnly = Boolean.parseBoolean(args[1]);
+        }
+
+        Logger.init(testOnly);
 
         Runtime runt = Runtime.instance();
         Profile profile = new ProfileImpl();
